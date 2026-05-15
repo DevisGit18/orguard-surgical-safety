24d9f64ddd92c3beb4bf9cc33d73ca436b629e1d
devisree-sumesh <devisreesumesh@gmail.com>
2026-05-15 20:22:13 +0300

    Fix wildcard route for path-to-regexp v8

--- backend/index.js
+++ backend/index.js
@@ -17,7 +17,7 @@ app.use('/api/checklists', checklistRoutes);
 
 // Serve frontend
 app.use(express.static(path.join(__dirname, '../frontend')));
-app.get('*', (req, res) => {
+app.get(/.*/, (req, res) => {
   res.sendFile(path.join(__dirname, '../frontend/index.html'));
 });
 
